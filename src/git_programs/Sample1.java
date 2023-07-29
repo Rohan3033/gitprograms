3423ac85157c1d6cc3544e57c90d2af4732b8020
Rohan3033 <rohantodkar911@gmail.com>
2023-07-29 10:55:52 +0530

    commit testing

--- src/git_programs/Sample1.java
+++ src/git_programs/Sample1.java
@@ -3,8 +3,9 @@ package git_programs;
 public class Sample1 {
 
 	public static void main(String[] args) {
-		System.out.println("Git program SampleS");
-
+		System.out.println("Git program Sample");
+		
+		System.out.println("Testing");
 	}
 
 }
